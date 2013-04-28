0603d3a9d07ac1466362fd86958e1ee3432be3e2
Don Pavlik <git@donpavlik.net>
2013-04-28 17:21:03 -0400

    Initial Commit

---
+++ Domain/Domain.h
@@ -0,0 +1,13 @@
+//
+//  Domain.h
+//  Domain
+//
+//  Created by Donald Pavlik on 4/28/13.
+//  Copyright (c) 2013 Donald Pavlik. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Domain : NSObject
+
+@end
---
+++ Domain/Domain.m
@@ -0,0 +1,13 @@
+//
+//  Domain.m
+//  Domain
+//
+//  Created by Donald Pavlik on 4/28/13.
+//  Copyright (c) 2013 Donald Pavlik. All rights reserved.
+//
+
+#import "Domain.h"
+
+@implementation Domain
+
+@end
